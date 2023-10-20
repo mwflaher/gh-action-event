934f2f4b4ba009e17a3f792f7a52cfb232530bd9
Matt Flaherty <mwflaher@alum.mit.edu>
2023-10-20 09:57:27 -0400

    just get tests passing

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import * as core from '@actions/core'
 import { isStringObject } from 'util/types'
 
-const RUDDERSTACK_WRITE_KEY: string =
-  process.env.RUDDERSTACK_WRITE_KEY || '2X1p3cBEe8JeaUkIrcgLgYWd6aa'
+// const RUDDERSTACK_WRITE_KEY: string =
+  // process.env.RUDDERSTACK_WRITE_KEY || '2X1p3cBEe8JeaUkIrcgLgYWd6aa'
 
 /**
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const event: string = core.getInput('name')
-    const user: string = core.getInput('user')
-    const properties: string = core.getInput('properties')
+    // const event: string = core.getInput('name')
+    // const user: string = core.getInput('user')
+    // const properties: string = core.getInput('properties')
 
     core.setOutput('success', 'true')
   } catch (error) {
