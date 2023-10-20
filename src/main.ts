8b750ecb3a17342f9ab45ea46536f06262fb9078
Matt Flaherty <mwflaher@alum.mit.edu>
2023-10-20 09:34:27 -0400

    Initial changes

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { isStringObject } from 'util/types'
+
+const RUDDERSTACK_WRITE_KEY: string =
+  process.env.RUDDERSTACK_WRITE_KEY || '2X1p3cBEe8JeaUkIrcgLgYWd6aa'
 
 /**
- * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const event: string = core.getInput('name')
+    const user: string = core.getInput('user')
+    const properties: string = core.getInput('properties')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('success', 'true')
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
