9507aeabe9b7c28ee3f3fe30818959e23d4cfa53
Matt Flaherty <mwflaher@alum.mit.edu>
2023-10-30 15:07:31 -0400

    fix prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Analytics from '@rudderstack/rudder-sdk-node'
 import { run } from './main'
 
-const RUDDERSTACK_DATAPLANE_URL: string = process.env.RUDDERSTACK_DATAPLANE_URL || ''
+const RUDDERSTACK_DATAPLANE_URL: string =
+  process.env.RUDDERSTACK_DATAPLANE_URL || ''
 const RUDDERSTACK_WRITE_KEY: string = process.env.RUDDERSTACK_WRITE_KEY || ''
 
 console.log(`Initializing client for ${process.env.RUDDERSTACK_DATAPLANE_URL}`)
@@ -9,7 +10,6 @@ console.log(`Initializing client for ${process.env.RUDDERSTACK_DATAPLANE_URL}`)
 const client = new Analytics(RUDDERSTACK_WRITE_KEY, {
   dataPlaneUrl: RUDDERSTACK_DATAPLANE_URL
 })
-
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 run(client)
