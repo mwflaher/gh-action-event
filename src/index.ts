1f5fd80b638aacdac52059a5c0cf6821d54eea06
Matt Flaherty <Matt Flaherty mwflaher@alum.mit.edu>
2023-10-29 23:22:27 -0400

    Env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import Analytics from '@rudderstack/rudder-sdk-node'
 import { run } from './main'
 
-const DATAPLANE_URL: string = process.env.DATAPLANE_URL || ''
+const RUDDERSTACK_DATAPLANE_URL: string =
+  process.env.RUDDERSTACK_DATAPLANE_URL || ''
 const RUDDERSTACK_WRITE_KEY: string = process.env.RUDDERSTACK_WRITE_KEY || ''
 
 const client = new Analytics(RUDDERSTACK_WRITE_KEY, {
-  dataPlaneUrl: DATAPLANE_URL
+  dataPlaneUrl: RUDDERSTACK_DATAPLANE_URL
 })
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
